8bedb2341e142e31e2c268669d6fd6b12f6e76b7
hchan7 <chaniam7@gmail.com>
2023-05-17 22:01:39 +0000

    more styling

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,8 @@ import Home from './Home';
 import Login from './Login';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginWithToken, logout, fetchUserPlaces, fetchPlaces } from '../store';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route, useLocation } from 'react-router-dom';
 import axios from 'axios';
-//import Form from './Form';
-//import Form2 from './Form2';
 //import { REACT_APP_GOOGLE_MAPS_API_KEY } from '../../secrets';
 import UserPlace from './UserPlace';
 import Places from './Places';
@@ -14,55 +12,40 @@ import Places from './Places';
 const App = ()=> {
   const { auth, places } = useSelector(state => state);
   const dispatch = useDispatch();
-  
+  const location = useLocation();
+  const view = location.pathname;
+  console.log('view:', view)
   useEffect(()=> {
     dispatch(loginWithToken());
   }, []);
-  // useEffect(()=> {
-  //   dispatch(fetchPlaces());
-  // }, []);
-  
+
+
   useEffect(()=>{
     if(auth.id){
       dispatch(fetchUserPlaces());
       dispatch(fetchPlaces());
     }
   }, [auth]);
-  // useEffect(()=>{
-  //   fetchData()
-  // },[]);
+
   return (
     <div>
       <h1>Vacation Bucket List</h1>
-      {/*{
-        auth.id ? <Home /> : <Login />
-      }*/}
-     {/* {
-        !!auth.id  && (
-          <div>
-            <nav>
-              <Link to='/'>Home</Link>
-            </nav>
-          </div>
-        )
-      }
-      */}
       {
         auth.id ? (
         <div>
-        {/*<div>
-            <nav>
-              <Link to='/places'>Community's Places</Link>
-            </nav>
-          </div>
-          */}
-          <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/places'>Community's Places</Link>
-            <div>
-              Welcome { auth.username }!!
-              <button onClick={()=> dispatch(logout())}>Logout</button>
-            </div>
+          <nav className="nav nav-underline">
+            <li className='nav-item'>
+              <Link to='/' className={ view === '/' ? 'selected nav-link' : ''}>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/places' className={ view === '/places' ? 'selected nav-link' : ''}>Community's Places</Link>
+            </li>
+            <li>
+              <div>
+                Welcome { auth.username }!!
+                <button onClick={()=> dispatch(logout())}>Logout</button>
+              </div>
+            </li>
           </nav>
         <Routes>
           <Route path='/' element={ <Home/>} />
